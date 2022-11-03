a577959dbf3bd14dd5d9e63e0a3d661e35994177
violette-git <ideasforyang@gmail.com>
2022-11-03 01:02:07 -0500

    test-11-3

--- foodtruckpersonalbe/settings.py
+++ foodtruckpersonalbe/settings.py
@@ -12,24 +12,25 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-from env.secret import DJANGO_KEY
+# from env.secret import DJANGO_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 import os
 import decouple
 import psycopg2
 import dj_database_url
 import django_heroku
+from django.conf import settings
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))] 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# DEBUG = True
+DEBUG = False
 
 # if DEBUG:
 
@@ -77,7 +78,7 @@ ROOT_URLCONF = 'foodtruckpersonalbe.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [str(BASE_DIR / 'templates')],
+        'DIRS': [str(BASE_DIR / 'templates'), str(BASE_DIR / 'static')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -153,7 +154,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath("static")), os.path.join(BASE_DIR, 'static')] 
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
